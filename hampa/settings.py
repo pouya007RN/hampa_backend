07449c8ef54eed71b67ced9b9bf07688f5439d39
pouya007RN <35175878+pouya007RN@users.noreply.github.com>
2020-08-15 13:06:06 +0430

    Update settings.py

--- hampa/settings.py
+++ hampa/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hampa2.co@gmail.com'
-EMAIL_HOST_PASSWORD = 'pobese1238'
+EMAIL_HOST_USER = '********@gmail.com'
+EMAIL_HOST_PASSWORD = '********'
 
 
 CORS_ALLOW_METHODS = (
